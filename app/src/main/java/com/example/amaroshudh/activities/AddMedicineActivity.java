da0a2755e28df6fab8153ba491c5d68a2d036fda
Jerry Jeriomio <jerryjoydhor19gmail.com>
2026-08-05 00:09:42 +0600

    add medicine screen added and working fine

--- app/src/main/java/com/example/amaroshudh/activities/AddMedicineActivity.java
+++ app/src/main/java/com/example/amaroshudh/activities/AddMedicineActivity.java
@@ -1,4 +1,18 @@
 package com.example.amaroshudh.activities;
 
-public class AddMedicineActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.amaroshudh.R;
+
+//tells that this class is a screen
+public class AddMedicineActivity extends AppCompatActivity {
+
+    @Override
+    //Runs when the screen opens.
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_medicine);
+    }
 }
--- app/src/main/java/com/example/amaroshudh/activities/MainActivity.java
+++ app/src/main/java/com/example/amaroshudh/activities/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.amaroshudh.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -24,7 +25,12 @@ public class MainActivity extends AppCompatActivity {
         FloatingActionButton fab = findViewById(R.id.fabAddMedicine);
 
         fab.setOnClickListener(v -> {
-            Toast.makeText(this, "Add Medicine Clicked!", Toast.LENGTH_SHORT).show();
+
+            Intent intent = new Intent(MainActivity.this,
+                    AddMedicineActivity.class);
+
+            startActivity(intent);
+
         });
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
